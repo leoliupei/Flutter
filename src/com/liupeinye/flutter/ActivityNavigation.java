c3c6fd4fcd8421a58a63808d1623d26a937f2881
liupeinye <liupei@appchina.com>
2013-11-06 13:46:21 +0800

    android init commit

---
+++ src/com/liupeinye/flutter/ActivityNavigation.java
@@ -0,0 +1,38 @@
+package com.liupeinye.flutter;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class ActivityNavigation extends ActionBarActivity {
+	
+	ActionBar mActionBar;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_navigation);
+		
+		mActionBar = getSupportActionBar();
+//		mActionBar.setDisplayHomeAsUpEnabled(true);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.activity_navigation, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		return super.onOptionsItemSelected(item);
+	}
+
+}
