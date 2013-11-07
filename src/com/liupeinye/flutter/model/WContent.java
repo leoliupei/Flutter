c554657be1f5d661ca1e230084ed36dcfed26144
liupeinye <liupei@appchina.com>
2013-11-07 19:33:46 +0800

    over

---
+++ src/com/liupeinye/flutter/model/WContent.java
@@ -0,0 +1,48 @@
+package com.liupeinye.flutter.model;
+
+public class WContent {
+	
+	/*
+	 * text : 微博内容,
+			origtext : 原始内容,
+			count : 微博被转次数,
+			mcount : 点评次数,
+			from : 来源,
+			fromurl : 来源url,
+			id : 微博唯一id,
+			image : 图片url列表,
+			video : 
+			{
+				picurl : 缩略图,
+				player : 播放器地址,
+				realurl : 视频原地址,
+				shorturl : 视频的短url,
+				title : 视频标题
+			},
+			music : 
+			{
+				author : 演唱者,
+				url : 音频地址,
+				title : 音频名字，歌名
+			},
+			name : 发表人帐户名,
+			openid : 用户唯一id，与name相对应,
+			nick : 发表人昵称,
+			self : 是否自已发的的微博，0-不是，1-是,
+			timestamp : 发表时间,
+			type : 微博类型，1-原创发表，2-转载，3-私信，4-回复，5-空回，6-提及，7-评论,
+			head : 发表者头像url,
+			location : 发表者所在地,
+			country_code : 国家码（其他时间线一样）,
+			province_code : 省份码（其他时间线一样）,
+			city_code : 城市码（其他时间线一样）,
+			isvip : 是否微博认证用户，0-不是，1-是,
+			geo : 发表者地理信息,
+			status : 微博状态，0-正常，1-系统删除，2-审核中，3-用户删除，4-根删除,
+			emotionurl : 心情图片url,
+			emotiontype : 心情类型,
+			source : 当type=2时，source即为源tweet
+	 * 
+	 * */
+
+}
---
+++ src/com/liupeinye/flutter/util/ResponseTimeLine.java
@@ -0,0 +1,441 @@
+package com.liupeinye.flutter.util;
+
+public class ResponseTimeLine {
+	
+	/*
+{
+	"errcode": 0,
+    "msg": "ok",
+    "ret": 0,
+    "seqid": 5943098276438489000,
+    "data": {
+    	"timestamp": 1383735397,
+        "hasnext": 0,
+        "info": [
+            {
+                "city_code": "",
+                "count": 501,
+                "country_code": "",
+                "emotiontype": 0,
+                "emotionurl": "",
+                "from": "QQ",
+                "fromurl": "http://im.qq.com\n",
+                "geo": "",
+                "head": "http://app.qlogo.cn/mbloghead/dfbeb4cae644229ab088",
+                "https_head": "https://app.qlogo.cn/mbloghead/dfbeb4cae644229ab088",
+                "id": "339940035221283",
+                "image": [
+                    "http://app.qpic.cn/mblogpic/578d320f639aa74d71fc"
+                ],
+                "isrealname": 1,
+                "isvip": 1,
+                "jing": "0",
+                "latitude": "0",
+                "location": "未知",
+                "longitude": "0",
+                "mcount": 65,
+                "music": null,
+                "name": "lol",
+                "nick": "英雄联盟",
+                "openid": "EBF42F017279B46AEC76031C2E7303D7",
+                "origtext": "#英雄姐说#这。。。死哥的大招吗？",
+                "pic": {
+                    "info": [
+                        {
+                            "pic_XDPI": [
+                                96
+                            ],
+                            "pic_YDPI": [
+                                96
+                            ],
+                            "pic_height": [
+                                100
+                            ],
+                            "pic_size": [
+                                14957
+                            ],
+                            "pic_type": [
+                                3
+                            ],
+                            "pic_width": [
+                                328
+                            ],
+                            "url": [
+                                "http://app.qpic.cn/mblogpic/578d320f639aa74d71fc"
+                            ]
+                        }
+                    ]
+                },
+                "province_code": "",
+                "readcount": 160871,
+                "self": 0,
+                "source": null,
+                "status": 0,
+                "text": "#英雄姐说#这。。。死哥的大招吗？",
+                "timestamp": 1383732161,
+                "type": 1,
+                "video": null,
+                "wei": "0"
+            },
+            {
+                "city_code": "",
+                "count": 13,
+                "country_code": "",
+                "emotiontype": 0,
+                "emotionurl": "",
+                "from": "腾讯微博",
+                "fromurl": "http://t.qq.com/\n",
+                "geo": "",
+                "head": "http://app.qlogo.cn/mbloghead/4457e4b426f3309169f6",
+                "https_head": "https://app.qlogo.cn/mbloghead/4457e4b426f3309169f6",
+                "id": "314479109852577",
+                "image": [
+                    "http://app.qpic.cn/mblogpic/d3ea36464c2839bcab70"
+                ],
+                "isrealname": 1,
+                "isvip": 1,
+                "jing": "0",
+                "latitude": "0",
+                "location": "未知",
+                "longitude": "0",
+                "mcount": 1,
+                "music": null,
+                "name": "qqwenhua",
+                "nick": "腾讯文化",
+                "openid": "A317501C2422BAD9F1C121133EB02092",
+                "origtext": "【重看傅雷之死：体制外生存艰难 被批斗对生绝望】傅雷的一生是与命运抗争的一生，“曾经奋斗，曾经痛苦，曾经流浪，曾经创造”；与克里斯朵夫不同的是，没有一个臂抱能让他歇一歇，所以他失去了为新的战斗再生的勇气。傅雷悲壮的一生是中国时代洪流中，知识分子命运的缩影。http://url.cn/NC9Mb3 ",
+                "pic": {
+                    "info": [
+                        {
+                            "pic_XDPI": [
+                                1
+                            ],
+                            "pic_YDPI": [
+                                1
+                            ],
+                            "pic_height": [
+                                485
+                            ],
+                            "pic_size": [
+                                126272
+                            ],
+                            "pic_type": [
+                                3
+                            ],
+                            "pic_width": [
+                                450
+                            ],
+                            "url": [
+                                "http://app.qpic.cn/mblogpic/d3ea36464c2839bcab70"
+                            ]
+                        }
+                    ]
+                },
+                "province_code": "",
+                "readcount": 5036,
+                "self": 0,
+                "source": null,
+                "status": 0,
+                "text": "【重看傅雷之死：体制外生存艰难 被批斗对生绝望】傅雷的一生是与命运抗争的一生，“曾经奋斗，曾经痛苦，曾经流浪，曾经创造”；与克里斯朵夫不同的是，没有一个臂抱能让他歇一歇，所以他失去了为新的战斗再生的勇气。傅雷悲壮的一生是中国时代洪流中，知识分子命运的缩影。http://url.cn/NC9Mb3</a> ",
+                "timestamp": 1383731434,
+                "type": 1,
+                "video": null,
+                "wei": "0"
+            },
+            {
+                "city_code": "5",
+                "count": 138,
+                "country_code": "1",
+                "emotiontype": 0,
+                "emotionurl": "",
+                "from": "iPhone客户端",
+                "fromurl": "http://t.qq.com/client.php?t=iphone\n",
+                "geo": "",
+                "head": "http://app.qlogo.cn/mbloghead/39ec35fc12ed8d35a3fc",
+                "https_head": "https://app.qlogo.cn/mbloghead/39ec35fc12ed8d35a3fc",
+                "id": "350440023131226",
+                "image": null,
+                "isrealname": 1,
+                "isvip": 1,
+                "jing": "0",
+                "latitude": "0",
+                "location": "中国 江苏 苏州",
+                "longitude": "0",
+                "mcount": 8,
+                "music": null,
+                "name": "Yami66",
+                "nick": "PLU_小米",
+                "openid": "39E3E1C087366885240BDC0E638653E3",
+                "origtext": "哈哈哈哈 || @bingdian9du: 。。。",
+                "pic": {
+                    "info": [
+                        {
+                            "pic_XDPI": [
+                                96
+                            ],
+                            "pic_YDPI": [
+                                96
+                            ],
+                            "pic_height": [
+                                219
+                            ],
+                            "pic_size": [
+                                2010656
+                            ],
+                            "pic_type": [
+                                2
+                            ],
+                            "pic_width": [
+                                249
+                            ],
+                            "url": [
+                                "http://app.qpic.cn/mblogpic/afea895a4ca03173d8cc"
+                            ]
+                        }
+                    ]
+                },
+                "province_code": "32",
+                "readcount": 12574,
+                "self": 0,
+                "source": {
+                    "city_code": "",
+                    "count": 307,
+                    "country_code": "",
+                    "from": "腾讯微博",
+                    "fromurl": "http://t.qq.com/\n",
+                    "geo": "",
+                    "head": "http://app.qlogo.cn/mbloghead/4a2556898a5d3ebdb788",
+                    "https_head": "https://app.qlogo.cn/mbloghead/4a2556898a5d3ebdb788",
+                    "id": "282411010525824",
+                    "image": [
+                        "http://app.qpic.cn/mblogpic/afea895a4ca03173d8cc"
+                    ],
+                    "isvip": 0,
+                    "jing": "0",
+                    "latitude": "0",
+                    "location": "未知",
+                    "longitude": "0",
+                    "mcount": 23,
+                    "music": null,
+                    "name": "junshine9233",
+                    "nick": "菜花头",
+                    "openid": "984BD967D25DFD6FEE233BEE82AE5691",
+                    "origtext": "#论演员的自我修养#团战可以输Timor必须屎。",
+                    "province_code": "",
+                    "readcount": 23199,
+                    "self": 0,
+                    "status": 0,
+                    "text": "#论演员的自我修养#团战可以输Timor必须屎。",
+                    "timestamp": 1383726998,
+                    "type": 1,
+                    "video": null,
+                    "wei": "0"
+                },
+                "status": 0,
+                "text": "哈哈哈哈 || @bingdian9du: 。。。",
+                "timestamp": 1383729783,
+                "type": 2,
+                "video": null,
+                "wei": "0"
+            },
+            {
+                "city_code": "5",
+                "count": 25,
+                "country_code": "1",
+                "emotiontype": 0,
+                "emotionurl": "",
+                "from": "iPhone客户端",
+                "fromurl": "http://t.qq.com/client.php?t=iphone\n",
+                "geo": "",
+                "head": "http://app.qlogo.cn/mbloghead/39ec35fc12ed8d35a3fc",
+                "https_head": "https://app.qlogo.cn/mbloghead/39ec35fc12ed8d35a3fc",
+                "id": "340224084409910",
+                "image": null,
+                "isrealname": 1,
+                "isvip": 1,
+                "jing": "0",
+                "latitude": "0",
+                "location": "中国 江苏 苏州",
+                "longitude": "0",
+                "mcount": 12,
+                "music": null,
+                "name": "Yami66",
+                "nick": "PLU_小米",
+                "openid": "39E3E1C087366885240BDC0E638653E3",
+                "origtext": "可以的。。。今晚直播又有新花样了",
+                "pic": {
+                    "info": [
+                        {
+                            "pic_XDPI": [
+                                96
+                            ],
+                            "pic_YDPI": [
+                                96
+                            ],
+                            "pic_height": [
+                                305
+                            ],
+                            "pic_size": [
+                                47999
+                            ],
+                            "pic_type": [
+                                3
+                            ],
+                            "pic_width": [
+                                551
+                            ],
+                            "url": [
+                                "http://app.qpic.cn/mblogpic/1abca8ebbfe72320e126"
+                            ]
+                        }
+                    ]
+                },
+                "province_code": "32",
+                "readcount": 12978,
+                "self": 0,
+                "source": {
+                    "city_code": "",
+                    "count": 26,
+                    "country_code": "",
+                    "from": "QQ",
+                    "fromurl": "http://im.qq.com\n",
+                    "geo": "",
+                    "head": "http://app.qlogo.cn/mbloghead/7ba5174999a1068c7082",
+                    "https_head": "https://app.qlogo.cn/mbloghead/7ba5174999a1068c7082",
+                    "id": "328221078506263",
+                    "image": [
+                        "http://app.qpic.cn/mblogpic/1abca8ebbfe72320e126"
+                    ],
+                    "isvip": 1,
+                    "jing": "0",
+                    "latitude": "0",
+                    "location": "未知",
+                    "longitude": "0",
+                    "mcount": 15,
+                    "music": null,
+                    "name": "boudoirhoney",
+                    "nick": "美杜莎女王",
+                    "openid": "F6F0B92C451DCC6AF1CE8D4415B7A1F3",
+                    "origtext": "这样的话对手怎么上高地？",
+                    "province_code": "",
+                    "readcount": 16315,
+                    "self": 0,
+                    "status": 0,
+                    "text": "这样的话对手怎么上高地？",
+                    "timestamp": 1383728974,
+                    "type": 1,
+                    "video": null,
+                    "wei": "0"
+                },
+                "status": 0,
+                "text": "可以的。。。今晚直播又有新花样了",
+                "timestamp": 1383729153,
+                "type": 2,
+                "video": null,
+                "wei": "0"
+            }
+        ],
+        "user": {
+            "AC_hank": "Hank",
+            "Health_Wang": "PLU_Health",
+            "JoKer874": "JoKer",
+            "Neotv_BaoBao": "傅若真_BaoBao",
+            "Sherry_C": "虎妞丶",
+            "TFT_wAwa": "闫紫境wAwa",
+            "TGArena": "腾讯游戏竞技平台",
+            "Yami66": "PLU_小米",
+            "a86555349": "tsukasa",
+            "apollo_gemini": "apollolost",
+            "bingdian9du": "冰点九度",
+            "boudoirhoney": "美杜莎女王",
+            "canyzhang": "小苍mm",
+            "dukenicholas": "PLU_华章DukE",
+            "fifaonline3": "FIFAOnline3",
+            "junshine9233": "菜花头",
+            "kolokdell": "戴老师",
+            "lol": "英雄联盟",
+            "mura1mama": "水水",
+            "nba2kol": "NBA2KOL",
+            "plu_xiaoyu03": "PLU_小于",
+            "plufriend": "陈琦栋PLUFriend",
+            "plulol2005": "PLU英雄联盟",
+            "qqcampus": "腾讯校园",
+            "qqspeed": "QQ飞车",
+            "qqtech": "腾讯科技",
+            "qqwenhua": "腾讯文化",
+            "susu027yan": "苏小妍",
+            "warface": "战争前线",
+            "xiaofengSS": "冯伟华PLU_WilL",
+            "xiaolou_": "小楼君",
+            "yjx12341": "Misaya若风"
+        }
+    }
+}
+中文说明
+{
+	errcode : 返回错误码,
+	msg : 错误信息,
+	ret : 返回值，0-成功，非0-失败,
+	data : 
+	{
+		timestamp : 服务器时间戳，不能用于翻页,
+		hasnext : 0-表示还有微博可拉取，1-已拉取完毕,
+		info : 
+		{
+			text : 微博内容,
+			origtext : 原始内容,
+			count : 微博被转次数,
+			mcount : 点评次数,
+			from : 来源,
+			fromurl : 来源url,
+			id : 微博唯一id,
+			image : 图片url列表,
+			video : 
+			{
+				picurl : 缩略图,
+				player : 播放器地址,
+				realurl : 视频原地址,
+				shorturl : 视频的短url,
+				title : 视频标题
+			},
+			music : 
+			{
+				author : 演唱者,
+				url : 音频地址,
+				title : 音频名字，歌名
+			},
+			name : 发表人帐户名,
+			openid : 用户唯一id，与name相对应,
+			nick : 发表人昵称,
+			self : 是否自已发的的微博，0-不是，1-是,
+			timestamp : 发表时间,
+			type : 微博类型，1-原创发表，2-转载，3-私信，4-回复，5-空回，6-提及，7-评论,
+			head : 发表者头像url,
+			location : 发表者所在地,
+			country_code : 国家码（其他时间线一样）,
+			province_code : 省份码（其他时间线一样）,
+			city_code : 城市码（其他时间线一样）,
+			isvip : 是否微博认证用户，0-不是，1-是,
+			geo : 发表者地理信息,
+			status : 微博状态，0-正常，1-系统删除，2-审核中，3-用户删除，4-根删除,
+			emotionurl : 心情图片url,
+			emotiontype : 心情类型,
+			source : 当type=2时，source即为源tweet
+		},
+		user : 
+		{
+			name : nick 当页数据涉及到的用户的帐号与昵称映射
+		}
+	},
+	seqid : 序列号
+}
+
+	 */
+	
+	public int errcode;			//返回错误码
+	public String msg;			//错误信息
+	public int ret;				//返回值 0-成功 非0-失败
+	public long seqid;			//序列号
+	public boolean hasnext;		//之后还有
+	public long timestamp;		//服务器时间戳，不能用于翻页
+}
